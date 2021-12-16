c97c4ad1b630d8a954e194992e9ff608dbd57b42
EngelFlores <engel.tads@gmail.com>
2021-12-16 01:38:27 -0300

    Added endpoints to api

--- src/api/githubService.js
+++ src/api/githubService.js
@@ -4,7 +4,37 @@ const baseUrl = "https://api.github.com"
 
 export default {
   async getStarredRepositories(userName) {
-    const url = `${baseUrl}/users/${userName}/starred`
-    return await axios.get(url);
+    try {
+      const url = `${baseUrl}/users/${userName}/starred`
+      return await axios.get(url);
+    } catch (error) {
+      console.error(error);
+    }
   },
+
+  async starRepository(repoOwner, repoName) {
+    try {
+      const url = `${baseUrl}/user/starred/${repoOwner}/${repoName}`
+      const token = process.env.REACT_APP_API_KEY
+      const config = {
+        headers: { "Accept": "application/vnd.github.v3+json", "Authorization": `token ${token}` }
+      }
+      return await axios.put(url, { owner: repoOwner, repo: repoName }, config)
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  async checkStarredRepository(repoOwner, repoName) {
+    try {
+      const url = `${baseUrl}/user/starred/${repoOwner}/${repoName}`
+      const token = process.env.REACT_APP_API_KEY
+      const config = {
+        headers: { "Accept": "application/vnd.github.v3+json", "Authorization": `token ${token}` }
+      }
+      return (await axios.get(url, config)).status
+    } catch (error) {
+      console.error(error);
+    }
+  }
 };

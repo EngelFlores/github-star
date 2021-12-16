4f6b4b0ea5f11b29c32f6e13925b80c401b39891
EngelFlores <engel.tads@gmail.com>
2021-12-16 03:45:30 -0300

    Edit search box

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -4,9 +4,9 @@ import "./SearchBox.css"
 const SearchBox = ({ setUserName, searchFn, disabled }) => {
   return (
     <div className="search-box">
-      <label htmlFor="user" className="search-box__label">User Name</label>
-      <input className="search-box__input" onChange={(event) => setUserName(event.target.value)} type="text" id="user" placeholder="ddavison"/>
-      <button onClick={searchFn} className="search-box__button" disabled={disabled}>Search</button>
+      <label data-testid="input-label" htmlFor="user" className="search-box__label">User Name</label>
+      <input data-testid="input" className="search-box__input" onChange={(event) => setUserName(event.target.value)} type="text" id="user" placeholder="ddavison"/>
+      <button data-testid="button" onClick={searchFn} className="search-box__button" disabled={disabled}>Search</button>
     </div>
   )
 }
